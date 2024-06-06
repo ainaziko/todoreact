61f203767bddfd1c9429c8580c76233db2e46326
Ainazik Momunalieva <58284183+ainaziko@users.noreply.github.com>
2024-06-06 08:08:14 +0600

    tasks are saved and retrieved from local storage now

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Greeting from "./components/Greeting";
 import TaskForm from "./components/TaskForm";
 import TaskList from "./components/TaskList";
 import './styles/index.css';
+import { saveTaskToLocalStorage, getTasksFromLocalStorage } from "./utils/localStorage";
+
 
 function App() {
   const [tasks, setTasks] = useState([]);
 
+  useEffect(() => {
+    try {
+        const storedTasks = getTasksFromLocalStorage();
+        if (storedTasks) {
+            setTasks(storedTasks);
+        }
+    } catch (error) {
+        console.error("Error loading tasks from local storage:", error);
+    }
+}, []);
 
-  const handleAddTask = (task) => {
-    //store in local storage
-    setTasks([...tasks, task])
-    console.log(task);
-  }
+
+const handleAddTask = (task) => {
+  setTasks(prevTasks => {
+      const updatedTasks = [...prevTasks, task];
+      saveTaskToLocalStorage(updatedTasks);
+      console.log('save', updatedTasks);
+      return updatedTasks;
+  });
+  console.log(task);
+};
+
 
   const handleStrikeTask = (index) => {
     const newTasks = [...tasks];
@@ -22,6 +40,7 @@ function App() {
 
   const handleDeleteTask = (index) => {
     setTasks(tasks.filter((_, i) => i !== index));
+    //del from local storage
   }; 
 
 
--- src/utils/localStorage.js
+++ src/utils/localStorage.js
@@ -6,3 +6,13 @@ export const getUsernameFromLocalStorage = () => {
     const savedName = JSON.parse(localStorage.getItem('username'));
     return savedName !== null ? savedName : '';
 }
+
+export const saveTaskToLocalStorage = (tasks) => {
+    if(tasks === null) return;
+    localStorage.setItem('todolist', JSON.stringify(tasks));
+}
+
+export const getTasksFromLocalStorage = () => {
+    const tasks = JSON.parse(localStorage.getItem('todolist'));
+    return tasks !== null ? tasks : '';
+}
